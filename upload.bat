d2402626ac947c8f9e0c58e476321102bda241e7
denox99 <deniraja53@guru.sd.belajar.id>
2026-04-23 20:35:33 +0700

    Update website config and files

--- upload.bat
+++ upload.bat
@@ -4,10 +4,16 @@ echo Menghubungkan ke repository GitHub...
 git init
 git remote remove origin >nul 2>&1
 git remote add origin https://github.com/sdnkejuron/website.git
+
+echo Mengambil update terbaru dari GitHub...
+git pull origin main --rebase
+
+echo Menambahkan perubahan lokal...
 git add .
-git commit -m "Upload semua proyek ke GitHub"
-git branch -M main
+git commit -m "Update website config and files"
+
 echo Mengunggah ke GitHub...
 git push -u origin main
+
 echo Selesai!
 pause
