e1972d77e98586b0425fca6da4909c177d2cc7de
sdnkejuron <sdnkejuron13@gmail.com>
2026-04-25 00:58:18 +0700

    Update workflow ke versi terbaru & ubah base url ke ruang.informasi

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/website/',
+    base: '/ruang.informasi/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
